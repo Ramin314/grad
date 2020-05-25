b79b95af320d011f7dde66c6cb439ffe86b5e212
job9931 <59708286+job9931@users.noreply.github.com>
2020-05-25 21:15:08 +0100

    Update prepare_data.py

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -3,51 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 from datetime import date
-
-    
-def published_date(string):
-    
-    '''This function is specfic for the string format
-    in this dataset.
-    
-    Input:
-        string: string of published datae
-        
-    Algorithm:
-        Split string at letter 'T' and then split at '-'.
-        This returns a list containing the year, month and day.
-        Convert strings to int type.
-        Create date object instance with format (year,month,day)
-        
-    Returns:
-        datetime.date object containing the date of publication.'''
-    
-    date_string = string.split('T')[0].split('-') # split into list containing strings
-    return date(int(date_string[0]),int(date_string[1]),int(date_string[2])) #return datetime object (year,month,day)
-
-
-
-def trending_date(string):
-    
-    '''This function is specfic for the string format
-    in this dataset.
-    
-    Input:
-        string: string of published datae
-        
-    Algorithm:
-        Split string at '.'.
-        This returns a list containing the year, month and day.
-        Change year from '00xx' to '20xx' (eg. 0017 to 2017).
-        Convert strings to int type.
-        Create date object instance with format (year,month,day)
-        
-    Returns:
-        datetime.date object containing the date of publication.'''
-        
-    trend_string = string.split('.')
-    trend_string[0] = '20'+trend_string[0]
-    return date(int(trend_string[0]),int(trend_string[2]),int(trend_string[1]))
+  
 
 
 #load data
@@ -88,14 +44,34 @@ us['country'] = 'us'
 #Join data sets
 data = pd.concat([gb,ca,us],axis=0).reset_index(drop=True)
 
-#Transform the published time column and trending time column.
+
+#Create date and time columns
+data['publish_time'] = pd.to_datetime(data['publish_time'], errors='coerce', format='%Y-%m-%dT%H:%M:%S.%fZ')
+
+data = data[data['trending_date'].notnull()]
+data = data[data['publish_time'].notnull()]
+
+data.insert(4, 'publish_date', data['publish_time'].dt.date)
+data['publish_time'] = data['publish_time'].dt.time
+
+#splitting publish_time column into separate hour, minute and second columns
+data['publish_time'] = data['publish_time'].astype(str)
+data[['hour','minute','second']] = data.publish_time.str.split(":", expand=True).astype(int)
+
+
+#adding a column which represents number of days a video remained on trending, 
+#simply by counting instances of each video_id, as each entry represents a day on trending
+occurances = data.groupby(['video_id']).size()
+days_trending = occurances.to_frame(name = 'days_trending').reset_index()
+
+'''#Transform the published time column and trending time column.
 #Not interested in originals so just overwrite columns.
 data['publish_time'] = data['publish_time'].apply(published_date)
 data['trending_date'] = data['trending_date'].apply(trending_date)
 
 #create column which contains the number of days it took to trend
 data['days_till_trending'] = data['trending_date'] - data['publish_time'] #gives a column of data objects
-data['days_till_trending'] = data['days_till_trending'].apply(lambda x: x.days) #extracts the number of days from date object
+data['days_till_trending'] = data['days_till_trending'].apply(lambda x: x.days) #extracts the number of days from date object'''
 
 if __name__ == '__main__':
     data.to_csv('data.csv', index=False)
